91787a7a7cbe5717bb9aef39e0a8e10be3b8e12c
rowellz <ztrow787@gmail.com>
2025-02-24 17:34:25 -0500

    changed service names and text color to grey

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -85,43 +85,43 @@
 			<br>
 			<p>Current num points: {$numPoints}</p>
 			<input type="range" bind:value={$numPoints} min="8" max="50" />
-			animate <input type="checkbox" bind:checked={$isPointCountAnimating} />
+			<p>animate</p> <input type="checkbox" bind:checked={$isPointCountAnimating} />
 			<br>
 			<br>
-			animation speed({$pointCountAnimatingSpeed}ms)<input type="range" bind:value={$pointCountAnimatingSpeed} min="10" max="1000" step="1" disabled={!$isPointCountAnimating} />
+			<p>animation speed({$pointCountAnimatingSpeed}ms)</p><input type="range" bind:value={$pointCountAnimatingSpeed} min="10" max="1000" step="1" disabled={!$isPointCountAnimating} />
 			<br>
 			<br>
 			<br>
 			<p>Current number of rings: {$numRings}</p>
 			<input type="range" bind:value={$numRings} min="100" max="1000" step="1"/>
-			animate <input type="checkbox" bind:checked={$isRingCountAnimating} />
+			<p>animate</p> <input type="checkbox" bind:checked={$isRingCountAnimating} />
 			<br>
 			<br>
-			animation speed({$ringCountAnimatingSpeed}ms)<input type="range" bind:value={$ringCountAnimatingSpeed} min="10" max="1000" step="1" disabled={!$isRingCountAnimating} />
+			<p>animation speed({$ringCountAnimatingSpeed}ms)</p><input type="range" bind:value={$ringCountAnimatingSpeed} min="10" max="1000" step="1" disabled={!$isRingCountAnimating} />
 			<br>
 			<br>
 			<br>
 			<p>Current point size: {$size}</p>
 			<input type="range" bind:value={$size} min="0.1" max="1.5" step="0.1" disabled={$isAudioAnalyzing} />
-			animate <input type="checkbox" bind:checked={$isPointSizeAnimating} disabled={$isAudioAnalyzing} />
+			<p>animate</p> <input type="checkbox" bind:checked={$isPointSizeAnimating} disabled={$isAudioAnalyzing} />
 			<br>
 			<br>
-			animation speed({$pointSizeAnimatingSpeed}ms)<input type="range" bind:value={$pointSizeAnimatingSpeed} min="10" max="1000" step="1" disabled={!$isPointSizeAnimating || $isAudioAnalyzing} />
+			<p>animation speed({$pointSizeAnimatingSpeed}ms)</p><input type="range" bind:value={$pointSizeAnimatingSpeed} min="10" max="1000" step="1" disabled={!$isPointSizeAnimating || $isAudioAnalyzing} />
 			<br>
 			<br>
 			<br>
 			<p>Current ring size: {$ringSize}</p>
 			<input type="range" bind:value={$ringSize} min="1" max="20" step="1" />
-			animate <input type="checkbox" bind:checked={$isAnimating} />
+			<p>animate</p> <input type="checkbox" bind:checked={$isAnimating} />
 			<br>
 			<br>
-			animation speed({$animationSpeed}ms)<input type="range" bind:value={$animationSpeed} min="10" max="1000" step="1" disabled={!$isAnimating} />
+			<p>animation speed({$animationSpeed}ms)</p><input type="range" bind:value={$animationSpeed} min="10" max="1000" step="1" disabled={!$isAnimating} />
 			<br>
 			<br>
 			<br>
 			<p>Current color:</p>
 			<input type="color" bind:value={$color} />
-			Rainbow <input type="checkbox" bind:checked={$isRainbow} /> 
+			<p>Rainbow</p> <input type="checkbox" bind:checked={$isRainbow} /> 
 			<br>        
 			<br>
 		</div>
@@ -134,5 +134,8 @@
 		button {
 			margin-left: 10px;
 			padding: 5px 10px;
+		}
+		p, label, span, button {
+			color: grey;
 		}
 	</style>
